9beca67a80a9158be5842c507e6bb4a104c4e535
Felipe Moreira <felipe0365@gmail.com>
2024-11-25 10:28:53 -0300

    initial commit

---
+++ ms-order/ms-order/src/test/java/br/com/compassuol/ms_order/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.ms_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-stock/ms-stock/src/test/java/br/com/compassuol/ms_stock/MsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.ms_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
